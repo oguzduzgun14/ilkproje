fac06eb98f209868bb46d79047fd498cfcca18e8
oguzduzgun14 <oguzduzgun1414@gmail.com>
2021-09-28 14:05:14 +0300

    ghf

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,27 @@
+package ilkproje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("merhaba java");
+        System.out.print("oðuz");
+        System.out.print(" ");
+        System.out.println("düzgün");
+        System.out.println("muzaffer"+" "+"övün");
+        System.out.println("yaþ :"+38);
+        
+        
+        		
+        		
+        		
+        		
+        		
+        
+        
+       
+        
+        
+	}
+
+}
